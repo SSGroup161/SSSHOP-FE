908bfa5a0a40596b3fdb54ca19c28d02c2f1bf8a
SSGroup161 <shellasaukiag@gmail.com>
2024-07-18 17:22:14 +0700

    first commit

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+export default function NotFound() {
+    return (
+        <>
+            <div className="m-auto">404</div>
+        </>
+    );
+}
